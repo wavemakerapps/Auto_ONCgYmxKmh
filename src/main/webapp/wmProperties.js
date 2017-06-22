abbba1aacfc927153f8c22b0c4ebcc82af8a5f34
root <root@76e6cc14dc4a>
2017-06-22 07:14:59 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ONCgYmxKmh",
+  "homePage" : "Main",
+  "name" : "Auto_ONCgYmxKmh",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
